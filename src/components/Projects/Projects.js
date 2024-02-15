99ee2c919aa95d89abe398d2247f94bc3f7a4871
Vanja <91335509+kaoboj1234@users.noreply.github.com>
2024-02-15 13:57:47 +0100

    Update Projects.js

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import Particle from "../Particle";
+import ProjectCard from "./ProjectCards";
 
 
 function Projects() {
